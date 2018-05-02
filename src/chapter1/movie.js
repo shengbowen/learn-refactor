de99635ed59a07f777e4f41d7f9e25362c3db316
bruce.sheng <shengbowen@meizu.com>
2018-05-02 22:12:53 +0800

    chapter1 1.4 move getCharge

--- src/chapter1/movie.js
+++ src/chapter1/movie.js
@@ -3,6 +3,24 @@ class Movie {
     this.title = title;
     this.priceCode = priceCode;
   }
+
+  getCharge(daysRented) {
+    let thisAmount = 0;
+    switch(this.priceCode) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case Movie.CHILDREDN:
+        thisAmount += 1.5;
+        if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
 }
 
 Movie.CHILDREDN = 2;
--- src/chapter1/rental.js
+++ src/chapter1/rental.js
@@ -7,22 +7,7 @@ class Rental {
   }
 
   getCharge() {
-    let thisAmount = 0;
-    const { movie, daysRented } = this;
-    switch(movie.priceCode) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += daysRented * 3;
-        break;
-      case Movie.CHILDREDN:
-        thisAmount += 1.5;
-        if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
+    return this.movie.getCharge(this.daysRented);
   }
 
   getFrequentRenterPoints() {
