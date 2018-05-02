46fb565a96ad5a73a03bfb7e9a4a3773969d15c7
bruce.sheng <shengbowen@meizu.com>
2018-05-02 21:52:05 +0800

    chapter1 1.3 add htmlStatement

--- src/chapter1/customer.js
+++ src/chapter1/customer.js
@@ -25,6 +25,20 @@ class Customer {
     return result;
   }
 
+  htmlStatement() {
+    let result = `<h1>Rental Recored for <em>${this.name}</em></h1><p>\n`;
+    for (let i = 0, len = this.rentals.length; i < len; i++) {
+      const rental = this.rentals[i];
+      const { movie, daysRented } = rental;
+
+      result += `${movie.title}: ${rental.getCharge()}</br>\n`;
+    }
+
+    result += `</p><p>Amount owed is <em>${this.getTotalCharge()}</em></p>\n`;
+    result += `<p>You earned <em>${this.getTotalFrequentRenterPoints()}</em> frequent renter points</p>`;
+    return result;
+  }
+
   getTotalCharge() {
     return this.rentals.reduce((acc, cur) => {
       return acc + cur.getCharge();
