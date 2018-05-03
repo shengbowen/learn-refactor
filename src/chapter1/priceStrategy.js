bec81b0f5e7ab5c9a6467877986c83a754346b47
bruce.sheng <shengbowen@meizu.com>
2018-05-03 21:45:42 +0800

    chapter1 1.4 replace condition with polymorphism

--- src/chapter1/priceStrategy.js
+++ src/chapter1/priceStrategy.js
@@ -6,21 +6,7 @@ class Price {
   }
 
   getCharge(daysRented) {
-    let thisAmount = 0;
-    switch(this.getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += daysRented * 3;
-        break;
-      case Movie.CHILDREDN:
-        thisAmount += 1.5;
-        if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
+    throw new Error('getPriceCode must be rewrite');
   }
 }
 
@@ -28,17 +14,33 @@ class ChildrensPrice extends Price {
   getPriceCode() {
     return Movie.CHILDREDN;
   }
+
+  getCharge(daysRented) {
+    let thisAmount = 1.5;
+    if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
+    return thisAmount;
+  }
 }
 
 class NewReleasePrice extends Price {
   getPriceCode() {
     return Movie.NEW_RELEASE;
   }
+
+  getCharge(daysRented) {
+    return daysRented * 3;
+  }
 }
 
 class RegularPrice extends Price {
   getPriceCode() {
     return Movie.REGULAR;
+  }
+
+  getCharge(daysRented) {
+    let thisAmount = 2;
+    if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
+    return thisAmount;
   }
 }
 
