c125c7f7577ad4ab4128110a2f1a6cbd0bd8845c
bruce.sheng <shengbowen@meizu.com>
2018-05-03 21:40:52 +0800

    chapter1 1.4 move method getCharge

--- src/chapter1/priceStrategy.js
+++ src/chapter1/priceStrategy.js
@@ -4,6 +4,24 @@ class Price {
   getPriceCode() {
     throw new Error('getPriceCode must be rewrite');
   }
+
+  getCharge(daysRented) {
+    let thisAmount = 0;
+    switch(this.getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case Movie.CHILDREDN:
+        thisAmount += 1.5;
+        if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
 }
 
 class ChildrensPrice extends Price {
